e788a0061709bcf8f69d6748da5b24144cb1f580
sofia <sofia@iassessments.com>
2022-03-25 16:48:11 +0200

    update

--- player.py
+++ player.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template, send_file
 app = Flask(__name__)
 
 
-# @app.route('/001-poklon-razogrev.mp3')
-# def song_mp3():
-#     return send_file('./static/audio/001-poklon-razogrev.mp3', attachment_filename='song.mp3')
+@app.route('/001-poklon-razogrev.mp3')
+def track():
+    return send_file('./static/audio/001-poklon-razogrev.mp3', attachment_filename='001-poklon-razogrev.mp3')
 
 @app.route("/")
 def hello_world(name=None):
